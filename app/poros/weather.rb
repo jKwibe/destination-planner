d1a0ca4bd0fd99df3f1fb0e2494fc3c2cdd58abb
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-09-06 17:39:28 -0600

    Add weather poro and test.

---
+++ app/poros/weather.rb
@@ -0,0 +1,14 @@
+class Weather
+  attr_reader :max_temp,
+              :min_temp,
+              :temp,
+              :detailed_description,
+              :main_description
+  def initialize(data)
+    @max_temp = data[:main][:temp_max]
+    @min_temp = data[:main][:temp_min]
+    @temp = data[:main][:temp]
+    @detailed_description = data[:weather][0][:description]
+    @main_description = data[:weather][0][:main]
+  end
+end
